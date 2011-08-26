7e56f65526d8d287df9884464a79dfc891310b13
Daniel Truemper <truemped@googlemail.com>
2011-08-26 10:55:44 +0200

    module imports and the "install" method

---
+++ zerobelt/eventloop/__init__.py
@@ -0,0 +1,27 @@
+#
+# Copyright (c) 2011 Daniel Truemper truemped@googlemail.com
+#
+# __init__.py 26-Aug-2011
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+import .ioloop
+import .stack_context
+
+def install():
+    """Install the custom eventloop as the tornado eventloop.
+    """
+    import tornado.ioloop
+    from .ioloop import IOLoop
+    tornado.ioloop.IOLoop = IOLoop
